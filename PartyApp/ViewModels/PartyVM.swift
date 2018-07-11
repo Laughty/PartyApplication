288c602d5ee2330559cb671198ce1c2a2b5597f5
MateuszW <>
2018-07-11 15:59:08 +0200

    More details in party protocol

--- PartyApp/ViewModels/PartyVM.swift
+++ PartyApp/ViewModels/PartyVM.swift
@@ -12,20 +12,35 @@ import UIKit
  TODO:
  implementation
  - View model for describing party details
+ let id: String
+ let location: String
+ let time: Date
+ let title: String
+ let description: String
+ let image: UIImage
  */
 
 protocol PartyVMProtocol {
     var title: String {get set}
     var image: UIImage {get set}
+    var location: String {get set}
+    var time: Date {get set}
+    var description: String {get set}
 }
 
 class PartyVM: PartyVMProtocol {
+    var location: String
+    var time: Date
+    var description: String
     var title: String
     var image: UIImage
     
     init(party: Party){
         self.image = party.image
         self.title = party.title
+        self.location = party.location
+        self.description = party.description
+        self.time = party.time
     }
     
 }
