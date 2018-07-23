1cb5b973919a14f1d18cf7261133a20304b9531b
MateuszW <>
2018-07-23 15:34:07 +0200

    smaller video

--- PartyApp/ViewControllers/WelcomeVC.swift
+++ PartyApp/ViewControllers/WelcomeVC.swift
@@ -231,7 +231,7 @@ class WelcomeVC: DefaultViewController, UITextFieldDelegate {
     }
     func videoMakerPartyShaker(){
         
-        let videoURL = Bundle.main.url(forResource: "blurred_lights", withExtension: "mp4")!
+        let videoURL = Bundle.main.url(forResource: "onboard", withExtension: "mp4")!
         
         player = AVPlayer(url: videoURL)
         player?.actionAtItemEnd = .none
